7104a016cfa30c056b9bacba6868600397ed5a03
Yudi454 <lucasyudi445@gmail.com>
2025-04-30 17:10:47 -0300

    Se agregan cards completas a article

--- src/components/mainHome/Article.jsx
+++ src/components/mainHome/Article.jsx
@@ -1,14 +1,25 @@
+import { Button, Card, Col, Row } from "react-bootstrap";
 
-const Article = ({lenguajes}) => {
-
-    console.log(lenguajes);
-
+const Article = ({ lenguajes }) => {
+  console.log(lenguajes);
 
   return (
     <div>
-      <p>Article</p>
+      <Row>
+        {lenguajes.map((lenguaje) => (
+          <Col md={4} key={lenguaje.id}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={lenguaje.imagen} />
+              <Card.Body>
+                <Card.Title>{lenguaje.nombre}</Card.Title>
+                <Card.Text>{lenguaje.descripcion}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
-  )
-}
+  );
+};
 
-export default Article
+export default Article;
