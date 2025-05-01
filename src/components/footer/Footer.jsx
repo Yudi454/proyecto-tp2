822470e5dc316d2b2b0266694e04ce5d7a170dd3
Nicolasinigo <ninigo93@gmail.com>
2025-05-01 15:28:06 -0300

    agrego el footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,28 @@
-import Map from "./Map"
+import Map from "./Map";
 
 const Footer = () => {
   return (
-    <div>
-        <Map/>
+    <div
+      className="container-fluid d-flex flex-column align-items-center px-4 py-4"
+      style={{
+        backgroundImage: `url("https://codersfree.nyc3.cdn.digitaloceanspaces.com/posts/conoce-5-lenguajes-de-programacion-basicos.jpg")`,
+        backgroundPosition: 'center',
+        fontWeight: 'bold',
+        color:"blue",
+        backgroundSize: 'cover',
+        textShadow: "1px 1px 3px rgb(255, 255, 255)",
+      }}
+    >
+      <h3>UTN - FRT 2025</h3>
+      <p className="m-3" style={{color:"red"}}>
+        La facultad regional de tucuman es una unidad academica dependiente de
+        la universidad tecnologica nacional establecida en 1954 en forma
+        conjunta con las facultades de bahia blanca y la plata. su sede se
+        encuentra en la zona norte de la ciudad de san miguel de tucuman.
+      </p>
+      <Map />
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/footer/Map.jsx
+++ src/components/footer/Map.jsx
@@ -1,10 +1,9 @@
-
 const Map = () => {
   return (
-    <div>
-      <p>Map</p>
+    <div className="ratio ratio-16x9">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4234.41427812252!2d-65.19641747188689!3d-26.817558854438865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225c23b7b6e863%3A0x976c9adc5013942c!2sUniversidad%20Tecnol%C3%B3gica%20Nacional%20-%20Facultad%20Regional%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1746115398021!5m2!1ses!2sar"></iframe>
     </div>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
